5d0fdd77ce9a2e857ba129b792aa56ed459689fa
Javiera Rojas <jrojar@uc.cl>
2022-11-24 18:36:09 -0300

    Actializacion reporte.

--- Codigo/Visualizacion_datos_01.R
+++ Codigo/Visualizacion_datos_01.R
@@ -20,36 +20,37 @@ datos_demografia <- read_csv("Datos/Korean_demographics_2000-2022.csv")
 S <-datos_demografia %>% 
   filter(Region %in% "Seoul" ) %>% 
   aggregate(Tasa_de_nacimientos~Año, FUN = mean)
-
+S$Tasa_de_nacimientos=round(S$Tasa_de_nacimientos,2)
 S$Region = rep("Seoul",23)
 
 b <-datos_demografia %>% 
   filter(Region %in% "Busan" ) %>% 
   aggregate(Tasa_de_nacimientos~Año, FUN = mean)
-
+b$Tasa_de_nacimientos=round(b$Tasa_de_nacimientos,2)
 b$Region = rep("Busan",23)
 
 i <-datos_demografia %>% 
   filter(Region %in% "Incheon" ) %>% 
   aggregate(Tasa_de_nacimientos~Año , FUN = mean)
-
+i$Tasa_de_nacimientos=round(i$Tasa_de_nacimientos,2)
 i$Region = rep("Incheon",23)
 
 d <-datos_demografia %>% 
   filter(Region %in% "Daegu" ) %>% 
   aggregate(Tasa_de_nacimientos~Año, FUN = mean)
-  
+d$Tasa_de_nacimientos=round(d$Tasa_de_nacimientos,2)  
 d$Region = rep("Daegu",23)
 
 
 d2<-datos_demografia %>% 
   filter(Region %in% "Daejeon" ) %>% 
   aggregate(Tasa_de_nacimientos~Año, FUN = mean)
+d2$Tasa_de_nacimientos=round(d2$Tasa_de_nacimientos,2)
 d2$Region = rep("Daejeon",23)
 
 (datos2<-rbind(S,b,i,d,d2)) 
 datos2<-data.frame(datos2)
-View(datos2)
+
 
 #grafico
 paises <-c("Seoul" = "#052F61" ,  "Busan" = "#CE6633"  , "Incheon" = "#A50E82", "Daegu" = "#6A9E1F","Daejeon" = "#E87D37" )
@@ -78,30 +79,31 @@ ggsave("Figuras/Grafico_01-Tasa_de_nacimientos.png", width = 10, height = 7)
 S3 <-datos_demografia %>% 
   filter(Region %in% "Seoul" ) %>% 
   aggregate(Tasa_de_Fallecimientos~Año, FUN = mean)
-
+S3$Tasa_de_Fallecimientos=round(S3$Tasa_de_Fallecimientos,2)
 S3$Region = rep("Seoul",23)
 
 b3 <-datos_demografia %>% 
   filter(Region %in% "Busan" ) %>% 
   aggregate(Tasa_de_Fallecimientos~Año, FUN = mean)
-
+b3$Tasa_de_Fallecimientos=round(b3$Tasa_de_Fallecimientos,2)
 b3$Region = rep("Busan",23)
 
 i3 <-datos_demografia %>% 
   filter(Region %in% "Incheon" ) %>% 
   aggregate(Tasa_de_Fallecimientos~Año , FUN = mean)
-
+i3$Tasa_de_Fallecimientos=round(i3$Tasa_de_Fallecimientos,2)
 i3$Region = rep("Incheon",23)
 
 d3 <-datos_demografia %>% 
   filter(Region %in% "Daegu" ) %>% 
   aggregate(Tasa_de_Fallecimientos~Año, FUN = mean)
-
+d3$Tasa_de_Fallecimientos=round(d3$Tasa_de_Fallecimientos,2)
 d3$Region = rep("Daegu",23)
 
 d23<-datos_demografia %>% 
   filter(Region %in% "Daejeon" ) %>% 
   aggregate(Tasa_de_Fallecimientos~Año, FUN = mean)
+d23$Tasa_de_Fallecimientos=round(d23$Tasa_de_Fallecimientos,2)
 d23$Region = rep("Daejeon",23)
 
 datos3<-rbind(S3,b3,i3,d3,d23)
@@ -121,6 +123,7 @@ datos3|>
   scale_x_continuous(breaks = seq("2000", "2022", by = 2)) +
   ylim(c(0,10)) + 
   geom_line( size = 1)+
+  geom_vline(xintercept = c(2019,2022),color = "#666666", linetype="dashed",size = 0.5)+
   geom_point(size = 2) +
   theme(axis.text.x = element_text(angle = 0),legend.position = "bottom")+
   scale_color_manual(values = paises) 
@@ -131,35 +134,36 @@ ggsave("Figuras/Grafico_02-Tasa_de_fallecimientos.png", width = 10, height = 7)
 S4 <-datos_demografia %>% 
   filter(Region %in% "Seoul" ) %>% 
   aggregate(Tasa_de_divorcios~Año, FUN = mean)
-
+S4$Tasa_de_divorcios=round(S4$Tasa_de_divorcios,2)
 S4$Region = rep("Seoul",23)
 
 b4 <-datos_demografia %>% 
   filter(Region %in% "Busan" ) %>% 
   aggregate(Tasa_de_divorcios~Año, FUN = mean)
-
+b4$Tasa_de_divorcios=round(b4$Tasa_de_divorcios,2)
 b4$Region = rep("Busan",23)
 
 i4 <-datos_demografia %>% 
   filter(Region %in% "Incheon" ) %>% 
   aggregate(Tasa_de_divorcios~Año , FUN = mean)
-
+i4$Tasa_de_divorcios=round(i4$Tasa_de_divorcios,2)
 i4$Region = rep("Incheon",23)
 
 d4 <-datos_demografia %>% 
   filter(Region %in% "Daegu" ) %>% 
   aggregate(Tasa_de_divorcios~Año, FUN = mean)
-
+d4$Tasa_de_divorcios=round(d4$Tasa_de_divorcios,2)
 d4$Region = rep("Daegu",23)
 
 d24<-datos_demografia %>% 
   filter(Region %in% "Daejeon" ) %>% 
   aggregate(Tasa_de_divorcios~Año, FUN = mean)
+d24$Tasa_de_divorcios=round(d24$Tasa_de_divorcios,2)
 d24$Region = rep("Daejeon",23)
 
 (datos4<-rbind(S4,b4,i4,d4,d24)) 
 datos4<-data.frame(datos4)
-View(datos4)
+
 
 
 #grafico
@@ -175,6 +179,7 @@ datos4|>
   ylim(c(0,5)) + 
   geom_line( size = 1)+
   geom_point(size = 2) +
+  geom_vline(xintercept = c(2000,2003),color = "#666666", linetype="dashed",size = 0.5)+
   theme(axis.text.x = element_text(angle = 0),legend.position = "bottom")+
   scale_color_manual(values = paises) 
 ggsave("Figuras/Grafico_03-Tasa_de_divorcios.png", width = 10, height = 7)  
@@ -183,24 +188,26 @@ ggsave("Figuras/Grafico_03-Tasa_de_divorcios.png", width = 10, height = 7)
 S5 <-datos_demografia %>% 
   filter(Region %in% "Seoul" ) %>% 
   aggregate(Tasa_de_casamientos~Año, FUN = mean)
-
+S5$Tasa_de_casamientos=round(S5$Tasa_de_casamientos,2)
 S5$Region = rep("Seoul",23)
 
 b5 <-datos_demografia %>% 
   filter(Region %in% "Busan" ) %>% 
   aggregate(Tasa_de_casamientos~Año, FUN = mean)
-
+b5$Tasa_de_casamientos=round(b5$Tasa_de_casamientos,2)
 b5$Region = rep("Busan",23)
 
 i5 <-datos_demografia %>% 
   filter(Region %in% "Incheon" ) %>% 
   aggregate(Tasa_de_casamientos~Año , FUN = mean)
+i5$Tasa_de_casamientos=round(i5$Tasa_de_casamientos,2)
 
 i5$Region = rep("Incheon",23)
 
 d5 <-datos_demografia %>% 
   filter(Region %in% "Daegu" ) %>% 
   aggregate(Tasa_de_casamientos~Año, FUN = mean)
+d5$Tasa_de_casamientos=round(d5$Tasa_de_casamientos,2)
 
 d5$Region = rep("Daegu",23)
 
@@ -208,11 +215,13 @@ d5$Region = rep("Daegu",23)
 d25<-datos_demografia %>% 
   filter(Region %in% "Daejeon" ) %>% 
   aggregate(Tasa_de_casamientos~Año, FUN = mean)
+
+d25$Tasa_de_casamientos=round(d25$Tasa_de_casamientos,2)
 d25$Region = rep("Daejeon",23)
 
 (datos5<-rbind(S5,b5,i5,d5,d25)) 
 datos5<-data.frame(datos5)
-View(datos5)
+
 
 #grafico
 paises <-c("Seoul" = "#052F61" ,  "Busan" = "#CE6633"  , "Incheon" = "#A50E82", "Daegu" = "#6A9E1F","Daejeon" = "#E87D37" )
@@ -237,35 +246,41 @@ ggsave("Figuras/Grafico_04-Tasa_de_casamientos.png", width = 10, height = 7)
 S6 <-datos_demografia %>% 
   filter(Region %in% "Seoul" ) %>% 
   aggregate(Tasa_de_crecimiento_natural~Año, FUN = mean)
+S6$Tasa_de_crecimiento_natural=round(S6$Tasa_de_crecimiento_natural,2)
 
 S6$Region = rep("Seoul",23)
 
 b6 <-datos_demografia %>% 
   filter(Region %in% "Busan" ) %>% 
   aggregate(Tasa_de_crecimiento_natural~Año, FUN = mean)
+b6$Tasa_de_crecimiento_natural=round(b6$Tasa_de_crecimiento_natural,2)
 
 b6$Region = rep("Busan",23)
 
 i6 <-datos_demografia %>% 
   filter(Region %in% "Incheon" ) %>% 
   aggregate(Tasa_de_crecimiento_natural~Año , FUN = mean)
+i6$Tasa_de_crecimiento_natural=round(i6$Tasa_de_crecimiento_natural,2)
 
 i6$Region = rep("Incheon",23)
 
 d6 <-datos_demografia %>% 
   filter(Region %in% "Daegu" ) %>% 
   aggregate(Tasa_de_crecimiento_natural~Año, FUN = mean)
+d6$Tasa_de_crecimiento_natural=round(d6$Tasa_de_crecimiento_natural,2)
 
 d6$Region = rep("Daegu",23)
 
 d26<-datos_demografia %>% 
   filter(Region %in% "Daejeon" ) %>% 
   aggregate(Tasa_de_crecimiento_natural~Año, FUN = mean)
+d26$Tasa_de_crecimiento_natural=round(d26$Tasa_de_crecimiento_natural,2)
+
 d26$Region = rep("Daejeon",23)
 
 (datos6<-rbind(S6,b6,i6,d6,d26)) 
 datos6<-data.frame(datos6)
-View(datos6)
+
 
 
 #grafico
@@ -281,6 +296,7 @@ datos6|>
   ylim(c(-10,12)) + 
   geom_line( size = 1)+
   geom_point(size = 2) +
+  geom_vline(xintercept = c(2016,2022),color = "#666666", linetype="dashed",size = 0.5)+
   theme(axis.text.x = element_text(angle = 0),legend.position = "bottom")+
   scale_color_manual(values = paises) 
 
@@ -310,7 +326,6 @@ data.frame(Variables = desp,
                            "Corresponde al año de la fecha en que se registraron los datos") )%>%
   gt()%>%
   tab_header(title = "Descripción de variables a trabajar")
-ggsave("Figuras/Tabla_01.png", width = 10, height = 7)  
 
 # Tabla especificamente entre 2019 y 2022 (PANDEMIA)----
 
@@ -320,8 +335,9 @@ final <- "2022"
 # juntar datos entre los años
 
   
-(datosf <- cbind(datos2,datos3[2],datos4[2],datos5[2],datos6[2]))
+datosf <- cbind(datos2,datos3[2],datos4[2],datos5[2],datos6[2])
 datosf <- data.frame(datosf)
+names(datosf)<-c("Año","Nacimientos","Region","Fallecimientos","Divorcios","Casamientos","Crecimientos_natural")
 
 datosf%>%
   filter(Año >= inicio & Año <= final)%>%
@@ -330,15 +346,13 @@ datosf%>%
   tab_source_note(source_note = "Fuente: elaboración propia a partir de datos disponibles en Kaggle")%>%
   fmt_number(columns = Año,decimals = 0,use_seps = FALSE)%>%
   tab_options(row_group.background.color = "#999999") %>%
-  tab_spanner(label = "Tasas",columns = c("Tasa_de_nacimientos",
-                                          "Tasa_de_Fallecimientos",
-                                          "Tasa_de_divorcios",
-                                          "Tasa_de_casamientos",
-                                          "Tasa_de_crecimiento_natural"))
+  
+  
+  tab_spanner(label = "Tasas",columns = c("Nacimientos","Fallecimientos","Divorcios","Casamientos","Crecimientos_natural"))
 
 
 
-ggsave("Figuras/Tabla_02.png", width = 10, height = 7)  
+
 
 
 
